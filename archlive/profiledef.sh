d4e61981038f9e707c627f511fba4494771eb086
Apostolos Chalis <tolishalis@gmail.com>
2026-09-07 12:21:25 +0300

    Change profiledef.sh

--- archlive/profiledef.sh
+++ archlive/profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
+iso_name="UniOS"
 iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue DVD"
+iso_publisher="UniOS <https://unios-project.eu>"
+iso_application="UniOS Live/Rescue DVD"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
